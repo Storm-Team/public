8d7d95c0a4c1a8d14bb984c18b8c697b9689a7fb
erickstyle <erickdjzcf@gmail.com>
2016-03-25 19:50:57 -0300

    php

---
+++ outros.php
@@ -0,0 +1,93 @@
+<?php
+ob_start();
+require('config/database.php');
+require('config/bf3conn.php');
+include('config/config.php');
+include('config/languages/'.LANG.'.php');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="shortcut icon" href="./template/images/staticon.png">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo $serverinfo['1']; ?> Server stats</title>
+<link href="template/style.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+body,td,th {
+	font-family: "Conv_big_noodle_titling";
+}
+body {
+	background-image: url(template/images/ibg.jpg);
+}
+</style>
+<script src="template/functions.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div id="headspace"></div>
+<div id="menu">
+	<div class="menuin">
+		<?php echo MENU ?>
+   	</div>
+</div>
+<div class="spacer"></div>
+<center><section class="devblog base-middle">
+ <div class="base-middle-content">
+  
+ <div class="row">
+ <section style="width: 88.4%;" class="span8 box box-primary latest-stories">
+ <header>
+ <h1>Downloads:</h1>
+ </header>
+ 
+ <article style="background-color: rgba(29, 26, 26, 0.74);" class="box-content devblog-post ">
+ <div class="devblog-index-content battlelog-wordpress">
+ <p style="color: #FFF;font-size:26px"><br />
+   <strong> </strong><a data-cke-saved-href="https://thepiratebay.se/torrent/14017861" href="https://thepiratebay.se/torrent/14017861" rel="nofollow external" title="link">DOWNLOAD TORRENT</a></p></p>
+ <p style="color: #000000;font-size:26px"><a data-cke-saved-href="https://mega.nz/#!nIFl1ShJ!tCxUxm6BGkpyiD_uVzUyRAbrDV1Ehsabn3tyXjV_pEM" href="https://mega.nz/#!nIFl1ShJ!tCxUxm6BGkpyiD_uVzUyRAbrDV1Ehsabn3tyXjV_pEM" rel="nofollow external" title="link">DOWNLOAD PATCH</a></p>
+ <p style="color: #000FFF;font-size:16px"><br />
+ </p>
+ </div>
+ </article>
+ 
+ <article style="background-color: rgba(29, 26, 26, 0.74);" class="box-content devblog-post ">
+ <div class="devblog-index-content battlelog-wordpress">
+ <p style="color: #FFF;font-size:16px"><em>Requisitos do sistema:</em><br />
+   <strong>Sistema operacional: </strong>Windows® Vista, / Windows® 7 / Windows® 8 / o Windows® 8.1 / Windows® 10<br />
+   <strong>do processador: </strong>Core 2 Duo de 2,4 GHz ou Athlon X2 2,7 GHz<br />
+   <strong>RAM: </strong>2 GB<br />
+   <strong>placa de vídeo: </strong>compatível com DirectX 10.1 com 512 MB ​​de RAM (Réguas ATI Radeon 3000, 4000, 5000 ou 6000, com a ATI Radeon 3870 ou superior performance)<br />
+   <strong>placa de som: </strong>compatível com DirectX 9.0c<br />
+   <strong>espaço livre no disco rígido: </strong>35 GB</p>
+ </div>
+ </article>
+ 
+ <article style="background-color: rgba(29, 26, 26, 0.74);" class="box-content devblog-post ">
+ <div class="devblog-index-content battlelog-wordpress">
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/D3dF7RC1hGI" frameborder="0" allowfullscreen></iframe>
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/lfxfnP_KRDY" frameborder="0" allowfullscreen></iframe>
+ </div></article></section>
+ </div>
+ 
+ <div class="row">
+ <section style="width: 98.4%;" class="span8 box box-primary latest-stories">
+ <header>
+ <h1><br>
+ <br>
+ </h1>
+ </header>
+ <article style="background-color: rgba(29, 26, 26, 0.74);color: #FFF;" class="box-content devblog-post ">
+   <div class="devblog-index-content battlelog-wordpress"></div>
+ </article>
+ 
+ 
+ </section>
+  </div>
+ 
+ 
+ </div>
+ </section><br>
+<br></center>
+
+</body>
+</html>
